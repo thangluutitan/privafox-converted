a78b4aa77bcf1f41bef7cbac682b626af923e2c9
thanh.nguyenc <thanh.nguyenc@localhost>
2015-07-28 15:21:45 +0700

    Update Extensions

--- extensions/confvars.sh
+++ extensions/confvars.sh
@@ -3,6 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_NAME=mozilla
-MOZ_APP_DISPLAYNAME=Mozilla
+MOZ_APP_NAME=privacore
+MOZ_APP_DISPLAYNAME=Privacore
 MOZ_APP_VERSION=$MOZILLA_VERSION
