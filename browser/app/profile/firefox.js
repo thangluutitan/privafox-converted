fa50d52f13bb91e6ad67aed1aaa2e312320948c5
thangluu <edit@localhost>
2015-10-01 16:29:29 +0700

    Fixed can't open view source page

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1981,7 +1981,7 @@ pref("browser.pocket.enabled", false);
 //pref("browser.pocket.oAuthConsumerKey", "40249-e88c401e1b1f2242d9e441c4");
 //pref("browser.pocket.useLocaleList", true);
 //pref("browser.pocket.enabledLocales", "en-US de es-ES ja ja-JP-mac ru");
-//pref("view_source.tab", true);
+pref("view_source.tab", true);
 
 /*
 *Privafox : Auto Install uBlock 
